34cd5b8cf5a34baad53ec4fadad3bc6ef5487835
CamilaReyes97 <camila.reyesber97@gmail.com>
2021-05-20 19:35:12 -0500

    Mi primer repositorio

---
+++ js/jquery.js
@@ -0,0 +1,58 @@
+/* selectores$*/
+
+/*$(document).ready (function(){
+    alert('jquely esta')
+})*/
+
+
+
+$('.titulo').click(function(){
+    $(this).css('color','pink')
+})  
+  
+  $('.op1').dblclick(function(){
+      $(this).css('background','purple')
+  })
+  
+  
+
+  $('.op2').mouseout(function(){
+      $(this).css('font-size','16px')
+  })
+
+
+
+  $('.parrafo').mouseover(function(){
+    $(this).css('blackground','blue')
+    $(this).css('color','white')
+})
+
+$('.parrafo').mouseout(function(){
+    $(this).css('blackground','b')
+})
+
+$('.op3').dblclick(function(){
+    $(this).css('background','blue')
+})
+
+
+$('.op4').dblclick(function(){
+    $(this).css('background','purple')
+})
+
+$('.op5').mouseover(function(){
+    $(this).css('font-size','50px'); 
+  })
+  
+  $('.op6').mouseover(function(){
+    $(this).css('font-size','50px'); 
+  })
+
+  $('.op7').mouseover(function(){
+    $(this).css('font-size','50px'); 
+  })
+
+
+$('.op8').dblclick(function(){
+    $(this).css('background','purple')
+})
